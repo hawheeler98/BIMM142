e3bf370d1274dfc0d1ced441fa134a6c601922a4
Hailey Wheeler <haileywheeler@Haileys-MacBook-Pro.local>
2023-11-17 13:12:18 -0800

    Edit README locally on computer

---
+++ Class04/Class04 copy.R
@@ -0,0 +1,17 @@
+5+3
+5-3
+5*3
+5/3
+nums<-1:5
+nums
+df <- data.frame(nums,chars = letters[1:5],Logic=c(T,T,F,F,T))
+df[2,2]
+df$chars
+df$nums
+df$nums>3
+x[c(T,F,T,F,T)]
+x <- 1:5
+x[c(T,F,T,F,T)]
+View(df)
+
+df[-c(1,4),]
---
+++ Class04/Lab4_EC.R
@@ -0,0 +1,4 @@
+source("http://thegrantlab.org/misc/cdc.R")
+View(cdc)
+head(cdc$height)
+tail(cdc$height)
